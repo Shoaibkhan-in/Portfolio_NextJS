5e35729d7058322aa166ebc043e1de41ecf8665a
Shoaibkhan-in <shoaibkhan8379@gmail.com>
2024-10-26 18:07:10 +0530

    Initial Commit

---
+++ app/components/AboutSection.jsx
@@ -0,0 +1,118 @@
+"use client";
+import React, { useTransition, useState } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Html</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>Node.js</li>
+        <li>Express.js</li>
+        <li>React.js</li>
+        <li>Next.js</li>
+        <li>MongoDB</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Bachelor's Of Computer Science Graduate</li>
+        <li>From SPPU University with 8.63GPA</li>
+        <li>HSC(XII) in Science with 83.67%</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Languages",
+    id: "language",
+    content: (
+      <ul className="list-disc pl-2">
+        <ul className="list-disc pl-2">
+          Languages
+        </ul>
+        <ul className="list-disc pl-2" >
+          <li>English</li>
+          <li>Hindi</li>
+          <li>Marathi</li>
+        </ul>
+        <ul className="list-disc pl-2">
+          Technical Languages
+        </ul>
+        <ul className="list-disc pl-2">
+          <li>Java</li>
+          <li>JavaScript</li>
+          <li>C Programming Language</li>
+          <li>Python</li>
+        </ul>
+
+      </ul>
+    ),
+  },
+];
+
+const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
+  return (
+    <section className="text-white" id="about">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image src="/images/about-image.png" alt="" width={500} height={500} />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-base lg:text-lg">
+            Hello! I’m Soyab, I have a strong foundation in programming,
+            with proficiency in C Programming, Java, and Python. I have hands-on experience in web development,
+            particularly with the MERN stack.
+            This expertise allows me to build full-stack web applications that are both robust and scalable.
+            I am passionate about leveraging my technical skills to solve real-world problems
+            and am constantly looking to expand my knowledge and expertise.
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              {" "}
+              Skills{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              {" "}
+              Education{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("language")}
+              active={tab === "language"}
+            >
+              {" "}
+              Languages{" "}
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
---
+++ app/components/ProjectsSection.jsx
@@ -0,0 +1,128 @@
+"use client";
+import React, { useState, useRef } from "react";
+import ProjectCard from "./ProjectCard";
+import ProjectTag from "./ProjectTag";
+import { motion, useInView } from "framer-motion";
+
+const projectsData = [
+  {
+    id: 1,
+    title: "React Portfolio Website",
+    description: "Personal Portfolio Website Created Using Next.js",
+    image: "/images/projects/portfolio.png",
+    tag: ["All", "Web"],
+    gitUrl: "/",
+    previewUrl: "/",
+  },
+  {
+    id: 2,
+    title: "PassManage-Password Manager",
+    description: "A Password Manager to track all passwords at one place",
+    image: "/images/projects/PassManage.png",
+    tag: ["All", "Web"],
+    gitUrl: "https://github.com/Shoaibkhan-in/PassManage-Password-Manager",
+    previewUrl: "https://shoaibkhan-in.github.io/PassManage-Password-Manager/",
+  },
+  {
+    id: 3,
+    title: "Get Me A Coffee",
+    description: "A Patreon Clone for Fans to support their favorite creators.",
+    image: "/images/projects/Coffee.png",
+    tag: ["All", "Web"],
+    gitUrl: "https://github.com/Shoaibkhan-in/Get_Me_A_Coffee",
+    previewUrl: "/",
+  },
+  {
+    id: 4,
+    title: "Spotify(Clone)",
+    description: "Music Streaming Web Application",
+    image: "/images/projects/Spotify.png",
+    tag: ["All", "Mobile"],
+    gitUrl: "https://github.com/Shoaibkhan-in/Spotify-Music-Player-",
+    previewUrl: "https://spotifyclonebyshoaib.freewebhostmost.com/",
+  },
+  {
+    id: 5,
+    title: "Retro Games",
+    description: "A Gaming Platform For classic players to enjoy their childhood.",
+    image: "/images/projects/Games.png",
+    tag: ["All", "Web"],
+    gitUrl: "https://github.com/Shoaibkhan-in/Basic-Games-of-JavaScript",
+    previewUrl: "/",
+  },
+  {
+    id: 6,
+    title: "Todo-List",
+    description: "To keep track of all activities ",
+    image: "/images/projects/todo.png",
+    tag: ["All", "Web"],
+    gitUrl: "https://github.com/Shoaibkhan-in/Todo-List",
+    previewUrl: "https://shoaibkhan-in.github.io/Todo-List/",
+  },
+];
+
+const ProjectsSection = () => {
+  const [tag, setTag] = useState("All");
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
+  const handleTagChange = (newTag) => {
+    setTag(newTag);
+  };
+
+  const filteredProjects = projectsData.filter((project) =>
+    project.tag.includes(tag)
+  );
+
+  const cardVariants = {
+    initial: { y: 50, opacity: 0 },
+    animate: { y: 0, opacity: 1 },
+  };
+
+  return (
+    <section id="projects">
+      <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
+        My Projects
+      </h2>
+      <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
+        <ProjectTag
+          onClick={handleTagChange}
+          name="All"
+          isSelected={tag === "All"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="Web"
+          isSelected={tag === "Web"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="Mobile"
+          isSelected={tag === "Mobile"}
+        />
+      </div>
+      <ul ref={ref} className="grid md:grid-cols-3 gap-8 md:gap-12">
+        {filteredProjects.map((project, index) => (
+          <motion.li
+            key={index}
+            variants={cardVariants}
+            initial="initial"
+            animate={isInView ? "animate" : "initial"}
+            transition={{ duration: 0.3, delay: index * 0.4 }}
+          >
+            <ProjectCard
+              key={project.id}
+              title={project.title}
+              description={project.description}
+              imgUrl={project.image}
+              gitUrl={project.gitUrl}
+              previewUrl={project.previewUrl}
+            />
+          </motion.li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default ProjectsSection;
